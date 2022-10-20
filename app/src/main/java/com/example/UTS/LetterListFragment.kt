045833849f2deb71816ed5efada0b3bb5509a200
RizqNafian <Rizqinafian2810@gmail.com>
2022-10-20 12:34:25 +0700

    01

---
+++ app/src/main/java/com/example/UTS/LetterListFragment.kt
@@ -0,0 +1,85 @@
+package com.example.UTS
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.UTS.databinding.FragmentLetterListBinding
+
+class LetterListFragment : Fragment() {
+    // menyiapkan variable binding
+    private var _binding: FragmentLetterListBinding? = null
+    // mngeset variable binding
+    private val binding get() = _binding!!
+    // mengatur layaoutmanager agar tetap menggunakan RecyclerView
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // mengambil dan menampilkan layout
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        // mengatur linearlayoutmangaer sebagi yang pertama tampil
+        chooseLayout()
+    }
+    // memperbarui objek terikat jika fragment hancur
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+    // mengatur layoutmanager untuk recyclerView berdasar list
+    private fun chooseLayout() {
+        when (isLinearLayoutManager) {
+            true -> {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = LetterAdapter(requireContext())
+            }
+            false -> {
+                recyclerView.layoutManager = GridLayoutManager(context, 4)
+                recyclerView.adapter = LetterAdapter(requireContext())
+            }
+        }
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
+    }
+    // mengatur cara menagani interaksi dengan menu trpilih
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/UTS/WordListFragment.kt
@@ -0,0 +1,54 @@
+package com.example.UTS
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.UTS.databinding.FragmentWordListBinding
+
+// menampilkan RecyclerView dengan tombol pencarian
+class WordListFragment : Fragment() {
+    // mengatur akses varible dan arah pencarian
+    companion object {
+        val LETTER = "letter"
+        val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+    // menyiapkan variable binding
+    private var _binding: FragmentWordListBinding? = null
+    // mengeset variable binding
+    private val binding get() = _binding!!
+    private lateinit var letterId: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // mengambil nilai LETTER
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // mengambil dan menampilkan layout
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+        // menambahkan DividerItemDecoration diantara item
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+    }
+    // memperbarui objek terikat jika fragment hancur
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
